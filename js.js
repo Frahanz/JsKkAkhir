d144609e6e4ae0b60b729305640e97281d776d04
Frahanz <farhanrahmat321@gmail.com>
2024-01-20 19:26:29 +0700

    first commit

---
+++ js.js
@@ -0,0 +1,45 @@
+var registerButton = document.querySelector('.register-btn');
+
+registerButton.addEventListener('click', function() {
+  alert('Register button clicked!'); // Tampilkan alert saat tombol register di klik
+});
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {
+    myIndex = 1;
+  }
+  x[myIndex - 1].style.display = "block";
+  setTimeout(carousel, 3000);
+}
+
+function handleMouse(element, isMouseEnter) {
+  const titleElement = element.querySelector('.card-title');
+  if (isMouseEnter) {
+    element.style.backgroundColor = '#946b13'; // Ubah warna latar belakang saat mouse masuk
+    titleElement.textContent = 'Dibaca Baik Baik!'; // Ubah judul saat mouse masuk
+  } else {
+    element.style.backgroundColor = ''; // Kembalikan warna latar belakang saat mouse keluar
+    titleElement.textContent = 'Bahasa Pemrograman'; // Kembalikan judul saat mouse keluar
+  }
+}
+
+const footerButton = document.getElementById('footerButton');
+const popup = document.getElementById('popup');
+
+footerButton.addEventListener('click', () => {
+  popup.style.display = 'block';
+});
+
+function closePopup() {
+  popup.style.display = 'none';
+}
+
